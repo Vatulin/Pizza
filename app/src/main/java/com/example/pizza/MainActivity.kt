ad638575cfefebab0c4963fd725c4bd3e3c7cdf7
Vatulin <egorvatulin06@gmail.com>
2025-01-20 01:17:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pizza/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.pizza
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.pizza.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
+        binding.lifecycleOwner = this
+    }
+}
