0d123848ed03fe3cd429c00b060147d5776dff56
Vatulin <egorvatulin06@gmail.com>
2025-01-20 21:44:52 +0300

    version 0.4.1 -nav_menu

--- app/src/main/java/com/example/pizza/Profile.kt
+++ app/src/main/java/com/example/pizza/Profile.kt
@@ -5,6 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.pizza.databinding.FragmentProfileBinding
+import com.example.pizza.databinding.FragmentRootBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,8 +39,16 @@ class Profile : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val binding : FragmentProfileBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
+
+        binding.profileBackButton.setOnClickListener {
+            binding.root.findNavController().navigate(R.id.action_profile_to_rootFragment)
+        }
+
+
+
+        return binding.root
     }
 
     companion object {
